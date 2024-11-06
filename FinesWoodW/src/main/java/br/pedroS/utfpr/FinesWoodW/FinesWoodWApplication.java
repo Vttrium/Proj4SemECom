5f54bde353db9ff92df71cc1738cfe3734a92a4a
Vttrium <phsauthier@yahoo.com>
2024-11-06 09:47:54 -0300

    SpringBootInit

---
+++ FinesWoodW/src/main/java/br/pedroS/utfpr/FinesWoodW/FinesWoodWApplication.java
@@ -0,0 +1,13 @@
+package br.pedroS.utfpr.FinesWoodW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinesWoodWApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinesWoodWApplication.class, args);
+	}
+
+}
---
+++ FinesWoodW/src/test/java/br/pedroS/utfpr/FinesWoodW/FinesWoodWApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pedroS.utfpr.FinesWoodW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinesWoodWApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
